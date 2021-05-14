5280d5007ef72ded6672dd92eb482eaa7d4b10c2
Shakarr <risaaflo@gmail.com>
2021-05-14 17:30:44 +0200

    update

--- Aplicacion-MTG/src/main/java/es/iespuertodelacruz/magic/vista/App.java
+++ Aplicacion-MTG/src/main/java/es/iespuertodelacruz/magic/vista/App.java
@@ -24,32 +24,50 @@ public class App {
                     System.out.println("Escribe una de las opciones");
                     opcion = sn.nextInt();
      
-                    switch (opcion) {
-                        case 1:
-                        int opcion1;
-                            System.out.println("Bienvenido al modo administrador");
-                            System.out.println("Elige una opcion a realizar");
-                            System.out.println("0.Salir");
-                            System.out.println("1.Insertar una Carta");
-                            System.out.println("2.Modificar una Carta");
-                            System.out.println("3.Buscar una Carta");
-                            opcion1 = sn.nextInt();
-                            break;
-                        case 2:
-                            System.out.println("Has seleccionado la opcion 2");
-                            break;
-                        case 3:
-                            System.out.println("Has seleccionado la opcion 3");
-                            break;
-                        case 4:
-                            salir = true;
-                            break;
-                        default:
-                            System.out.println("Solo números entre 1 y 4");
+                    while(!salir){
+
+                        System.out.println("Bienvenido al modo administrador");
+                        System.out.println("Elige una opcion a realizar");
+                        System.out.println("0.Salir");
+                        System.out.println("1.Insertar una Carta");
+                        System.out.println("2.Modificar una Carta");
+                        System.out.println("3.Buscar una Carta");
+
+                        
+
+                        try {
+
+                            System.out.println("Escribe una de las opciones");
+                            int opcion1 = sn.nextInt();
+
+                            switch (opcion) {
+                                case 1:
+                                
+                                    
+                                    opcion1 = sn.nextInt();
+                                    break;
+                                case 2:
+                                    System.out.println("Has seleccionado la opcion 2");
+                                    break;
+                                case 3:
+                                    System.out.println("Has seleccionado la opcion 3");
+                                    break;
+                                case 4:
+                                    salir = true;
+                                    break;
+                                default:
+                                    System.out.println("Solo números entre 1 y 4");
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Debes insertar un número");
+                            salir=true;
+                        }
                     }
+                    
+                    
                 } catch (InputMismatchException e) {
                     System.out.println("Debes insertar un número");
-                    sn.next();
+                    salir=true;
                 }
             }
      
