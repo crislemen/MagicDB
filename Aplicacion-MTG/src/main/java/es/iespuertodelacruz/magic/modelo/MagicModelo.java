f18e346a35a319b803862955cb186c88a213e7eb
Shakarr <risaaflo@gmail.com>
2021-05-18 16:34:41 +0200

    update file

--- Aplicacion-MTG/src/main/java/es/iespuertodelacruz/magic/modelo/MagicModelo.java
+++ Aplicacion-MTG/src/main/java/es/iespuertodelacruz/magic/modelo/MagicModelo.java
@@ -1,5 +1,56 @@
 package es.iespuertodelacruz.magic.modelo;
+import es.iespuertodelacruz.magic.api.*;
+import es.iespuertodelacruz.magic.exception.PersistenciaException;
 
 public class MagicModelo {
     
+    BbDd persistencia;
+
+    public MagicModelo(){
+        persistencia = new BbDd("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/test"
+        , "minty", "greatsqldb");
+    }
+
+    /**
+     * Metodo encargado de insertar una carta
+     * @param carta objeto a insertar
+     * @throws PersistenciaException error controlado
+     */
+    public void insertarCarta(Carta carta) throws PersistenciaException{
+        persistencia.insertar(carta);
+    }
+
+    /**
+     * Metodo encargado de buscar uan carta por id
+     * @param carta objeto a buscar
+     * @throws PersistenciaException error controlado
+     */
+    public void buscarPorID(Carta carta) throws PersistenciaException{
+       persistencia.obtenerCartaId(carta.getId());
+    }
+
+    /**
+     * Metodo encargado de bsucar una carta por nombre
+     * @param carta objeto a buscar
+     * @throws PersistenciaException error controlado
+     */
+    public void buscarPorNombre(Carta carta) throws PersistenciaException{
+        persistencia.obtenerCartaNombre(carta.getNombreCarta());
+    }
+
+    /**
+     * Metodo encargado de buscar una carta por color
+     * @param carta objeto a buscar
+     * @throws PersistenciaException error controlado
+     */
+    public void buscarPorCosteDeMana(Carta carta) throws PersistenciaException{
+        persistencia.obtenerCartaMana(carta.getCosteMana());
+    }
+
+    /**
+     * Metodo encargado de buscar por el coste de mana de la carta
+     * @param carta objeto a buscar
+     * @throws PersistenciaException error controlado
+     */
+    public void buscarPorColor(Carta carta){}
 }
